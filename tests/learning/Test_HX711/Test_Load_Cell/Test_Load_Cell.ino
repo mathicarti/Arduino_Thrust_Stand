35696c9618be4281ad1242aa18336ce92798f9f0
Mathieu Cartier <100831252+mathicarti@users.noreply.github.com>
2025-03-22 21:35:24 +1000

    Added a way to calibrate load cell and save it to the Arduino EEPROM. Tested, code to read weight from load cell.

--- tests/learning/Test_HX711/Test_Load_Cell/Test_Load_Cell.ino
+++ tests/learning/Test_HX711/Test_Load_Cell/Test_Load_Cell.ino
@@ -1,20 +1,56 @@
-#include "HX711.h"
+#include <HX711_ADC.h>
+#include <EEPROM.h>
 
-HX711 myScale;
+//pins:
+const int HX711_dout = 4;
+const int HX711_sck = 5;
 
-int const cell_data_PIN = 4;
-int const cell_clock_PIN = 6;
+//HX711 constructor:
+HX711_ADC LoadCell(HX711_dout, HX711_sck);
 
-myScale.set_offset(68289);
-myScale.set_scale(1071.572875);
-
+const int calVal_eepromAdress = 0;
+unsigned long t = 0;
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(57600); delay(10);
+  LoadCell.begin();
+  Serial.println();
+  Serial.println("Calibrating Cell");
 
+  float calibrationValue; // Can recalibrate with Calibration.ino
+  calibrationValue = 696.0;
+
+  EEPROM.get(calVal_eepromAdress, calibrationValue); // Gets saved calibration value from EEPROM
+
+  unsigned long stabilizingtime = 3000; // time needed to get baseline weight
+  
+  boolean _tare = true;
+  LoadCell.start(stabilizingtime, _tare);
+  
+  if (LoadCell.getTareTimeoutFlag()) { // if any bad connection detected
+    Serial.println("Timeout, check MCU>HX711 wiring and pin designations");
+    while (1);
+  }
+  else {
+    LoadCell.setCalFactor(calibrationValue); // set calibration value
+    Serial.println("Startup is complete");
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  static boolean newDataReady = 0;
 
+  // check for new data/start next conversion and prints it
+  if (LoadCell.update() == 1) {
+    float weight = LoadCell.getData();
+      Serial.println(weight);
+      newDataReady = 0;
+      t = millis();
+  }
+
+  // if 't' sent, load cell tared
+  if (Serial.available() > 0) {
+    char inByte = Serial.read();
+    if (inByte == 't') LoadCell.tareNoDelay();
+  }
 }
